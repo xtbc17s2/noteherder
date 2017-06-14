08fc517b51ebed3814156603333b2f92ce44d881
Dave Strus <dave@getfretless.com>
2017-06-14 12:09:01 -0400

    Style the sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
+import quill from './quill.svg'
 import './Sidebar.css'
 
 const Sidebar = () => {
-  return <p>SIDEBAR!</p>
+  return (
+    <div className="Sidebar">
+      <div className="logo">
+        <img src={quill} alt="Noteherder" />
+      </div>
+      <button className="new-note">
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1152887/new-hover.png" alt="New note" />
+        <img className="outline" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1152887/new.png" alt="New note" />
+      </button>
+    </div>
+  )
 }
 
 export default Sidebar
