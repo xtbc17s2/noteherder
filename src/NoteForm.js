4f4a499136f7b1975b1e167128d336ed734cd24a
Dave Strus <dave@getfretless.com>
2017-06-14 16:34:49 -0400

    Add content and style to Sidebar

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 
+import './NoteForm.css'
+
 class NoteForm extends Component {
   render() {
     return <p>NOTE FORM!</p>
--- src/NoteList.js
+++ src/NoteList.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import './NoteList.css'
+
 const NoteList = () => {
   return <p>NOTE LIST!</p>
 }
