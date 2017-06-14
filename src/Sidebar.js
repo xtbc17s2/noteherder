ef6d927eecf3fa505e5e66d40903bde8cee12fc2
Dave Strus <dave@getfretless.com>
2017-06-14 16:29:53 -0400

    Load images in script

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,7 +1,21 @@
 import React from 'react'
 
+import quill from './quill.svg'
+import newHover from './new-hover.png'
+import newIcon from './new.png'
+
 const Sidebar = () => {
-  return <p>SIDEBAR!</p>
+  return (
+    <nav className="Sidebar">
+      <div className="logo">
+        <img src={quill} alt="Noteherder" />
+      </div>
+      <button className="new-note">
+        <img src={newHover} alt="New note" />
+        <img className="outline" src={newIcon} alt="New note" />
+      </button>
+    </nav>
+  )
 }
 
 export default Sidebar
