f36b85248538b694baf37b57f07b8fb501d8b88e
Dave Strus <dave@getfretless.com>
2017-06-15 10:07:39 -0400

    Add Note component and render (hardcoded) notes dynamically

--- src/App.js
+++ src/App.js
@@ -4,10 +4,29 @@ import './App.css';
 import Main from './Main'
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      notes: {
+        'note-1': {
+          id: 'note-1',
+          title: 'Thoughts on React',
+          body: 'React is pretty nifty. Declarative FTW! 🎸',
+        },
+        'note-2': {
+          id: 'note-2',
+          title: 'State and props?',
+          body: 'Wat',
+        },
+      },
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <Main />
+        <Main notes={this.state.notes} />
       </div>
     );
   }
