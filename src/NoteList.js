fd7cb4569a013bc15ca6476f4053aae45ed22c64
Dave Strus <dave@getfretless.com>
2017-06-15 09:36:24 -0400

    Add (static) content and style to NoteList

--- src/NoteList.js
+++ src/NoteList.js
@@ -3,7 +3,25 @@ import React from 'react'
 import './NoteList.css'
 
 const NoteList = () => {
-  return <p>NOTE LIST</p>
+  return (
+    <div className="NoteList">
+      <h3>Notes</h3>
+      <ul id="notes">
+        <li>
+          <div className="note">
+            <div className="note-title">
+              Citizens of distant epochs
+            </div>
+            <div className="note-body">
+              <p>
+                Sea of Tranquility the ash of stellar alchemy vastness is bearable only through love bits of moving fluff are creatures of the cosmos, consciousness a still more glorious dawn awaits two ghostly white figures in coveralls and helmets are soflty dancing tingling of the spine, concept of the number one brain is the seed of intelligence are creatures of the cosmos?
+              </p>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  )
 }
 
 export default NoteList
