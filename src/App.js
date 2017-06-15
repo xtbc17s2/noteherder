e365e4affefe6cad0f68ca64d58871cc2e7f3d9d
Dave Strus <dave@getfretless.com>
2017-06-15 11:22:31 -0400

    Save a note from the form

--- src/App.js
+++ src/App.js
@@ -23,10 +23,19 @@ class App extends Component {
     }
   }
 
+  saveNote = (note) => {
+    if (!note.id) {
+      note.id = `note-${Date.now()}`
+    }
+    const notes = {...this.state.notes}
+    notes[note.id] = note
+    this.setState({ notes })
+  }
+
   render() {
     return (
       <div className="App">
-        <Main notes={this.state.notes} />
+        <Main notes={this.state.notes} saveNote={this.saveNote} />
       </div>
     );
   }
--- src/NoteForm.js
+++ src/NoteForm.js
@@ -3,6 +3,32 @@ import React, { Component } from 'react'
 import './NoteForm.css'
 
 class NoteForm extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      note: this.blankNote(),
+    }
+  }
+
+  blankNote = () => {
+    return {
+      id: null,
+      title: '',
+      body: '',
+    }
+  }
+
+  handleChanges = (ev) => {
+    const note = {...this.state.note}
+    note[ev.target.name] = ev.target.value
+    this.setState(
+      { note },
+      () => this.props.saveNote(this.state.note)
+    )
+    
+  }
+
   render() {
     return (
       <div className="NoteForm">
@@ -12,14 +38,16 @@ class NoteForm extends Component {
               type="text"
               name="title"
               placeholder="Title your note"
+              onChange={this.handleChanges}
+              value={this.state.note.title}
             />
           </p>
           <p>
             <textarea
               name="body"
-              cols="30"
-              rows="10"
               placeholder="Just start typing..."
+              onChange={this.handleChanges}
+              value={this.state.note.body}
           ></textarea>
           </p>
         </form>
