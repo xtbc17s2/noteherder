a2e46fda239c84ffd7e5b104c12559b2b52aec3f
Dave Strus <dave@getfretless.com>
2017-06-15 16:03:58 -0400

    Save a note from the form

--- src/Main.js
+++ src/Main.js
@@ -10,7 +10,7 @@ const Main = (props) => {
     <div className="Main">
       <Sidebar />
       <NoteList notes={props.notes} />
-      <NoteForm />
+      <NoteForm saveNote={props.saveNote} />
     </div>
   )
 }
--- src/NoteForm.js
+++ src/NoteForm.js
@@ -3,6 +3,31 @@ import React, { Component } from 'react'
 import './NoteForm.css'
 
 class NoteForm extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      note: this.blankNote(),
+    }
+  }
+
+  blankNote = () => {
+    return {
+      id: null,
+      title: '',
+      body: '',
+    }
+  }
+
+  handleChanges = (ev) => {
+    const note = {...this.state.note}
+    note[ev.target.name] = ev.target.value
+    this.setState(
+      { note },
+      () => this.props.saveNote(this.state.note)
+    )
+  }
+
   render() {
     return (
       <div className="NoteForm">
@@ -12,12 +37,16 @@ class NoteForm extends Component {
               type="text"
               name="title"
               placeholder="Title your note"
+              onChange={this.handleChanges}
+              value={this.state.note.title}
             />
           </p>
           <p>
             <textarea
               name="body"
               placeholder="Just start typing..."
+              onChange={this.handleChanges}
+              value={this.state.note.body}
             ></textarea>
           </p>
         </form>
