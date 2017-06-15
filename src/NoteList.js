8229986cc6d4d0685af1622859f6eb159b41feec
Dave Strus <dave@getfretless.com>
2017-06-15 10:25:15 -0400

    Destructure props in stateless functional components

--- src/NoteList.js
+++ src/NoteList.js
@@ -3,13 +3,13 @@ import React from 'react'
 import './NoteList.css'
 import Note from './Note'
 
-const NoteList = (props) => {
+const NoteList = ({ notes }) => {
   return (
     <div className="NoteList">
       <h3>Notes</h3>
       <ul id="notes">
-        { Object.keys(props.notes).map((noteId) => {
-          return <Note note={props.notes[noteId]} key={noteId} />
+        { Object.keys(notes).map((noteId) => {
+          return <Note note={notes[noteId]} key={noteId} />
         }) }
       </ul>
     </div>
