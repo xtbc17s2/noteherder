8afdeb44776946a29869c7dff4bd1cc9a218e6fe
Dave Strus <dave@getfretless.com>
2017-06-15 15:05:26 -0400

    Fix indentation

--- src/NoteList.js
+++ src/NoteList.js
@@ -6,13 +6,13 @@ import Note from './Note'
 const NoteList = ({ notes }) => {
   return (
     <div className="NoteList">
-        <h3>Notes</h3>
-        <ul id="notes">
-          {Object.keys(notes).map((noteId) => {
-            return <Note note={notes[noteId]} key={noteId} />
-          })}
-        </ul>
-      </div>
+      <h3>Notes</h3>
+      <ul id="notes">
+        {Object.keys(notes).map((noteId) => {
+          return <Note note={notes[noteId]} key={noteId} />
+        })}
+      </ul>
+    </div>
   )
 }
 
