91e47403cdb8d00e7812da91258c2ae98bea89e1
Dave Strus <dave@getfretless.com>
2017-06-20 14:17:41 -0400

    Add Google authentication and update re-base example

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,20 +1,28 @@
 import React from 'react'
 
 import './SignIn.css'
-import { auth, githubProvider } from './base'
+import { auth, githubProvider, googleProvider } from './base'
 
 const SignIn = () => {
-  const authenticate = () => {
-    auth.signInWithPopup(githubProvider)
+  const authenticate = (provider) => {
+    auth.signInWithPopup(provider)
   }
 
   return (
-    <button
-      className="SignIn"
-      onClick={authenticate}
-    >
-      Sign In
-    </button>
+    <div>
+      <button
+        className="SignIn"
+        onClick={() => authenticate(githubProvider)}
+      >
+        Sign in with GitHub
+      </button>
+      <button
+        className="SignIn"
+        onClick={() => authenticate(googleProvider)}
+      >
+        Sign in with Google
+      </button>
+    </div>
   )
 }
 
--- src/base.example.js
+++ src/base.example.js
@@ -16,5 +16,6 @@ const db = database(app)
 
 export const auth = app.auth()
 export const githubProvider = new firebase.auth.GithubAuthProvider()
+export const googleProvider = new firebase.auth.GoogleAuthProvider()
 
 export default Rebase.createClass(db)
