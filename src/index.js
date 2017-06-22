f6f3f49ae571d02ee82ea41bb9738487fa57c5fb
Dave Strus <dave@getfretless.com>
2017-06-22 10:27:50 -0400

    Use HashRouter for GitHub Pages

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { HashRouter, Route } from 'react-router-dom'
 
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 ReactDOM.render(
-  <Router>
+  <HashRouter basename="/noteherder">
     <Route component={App} />
-  </Router>,
+  </HashRouter>,
   document.getElementById('root')
 );
 registerServiceWorker();
